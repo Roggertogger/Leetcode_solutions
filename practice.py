b4d8257aef6438221c4524bdcbcb3e7db09f3d32
Roggertogger <mamunofficialbox@gmail.com>
2025-01-24 01:51:49 +0600

    append and insert learned

--- practice.py
+++ practice.py
@@ -7,5 +7,13 @@ while i <= 5:
 
 # for loop
 
-for i in range(10):
+for i in range(2):
     print("hello world")
+
+# append and insert
+
+mark = [21, 23, 35, 36]
+
+mark.append(32)
+mark.insert(2,43)
+print(mark)
