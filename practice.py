f31c453459755aae78e4e83294bf59b7b2bd7760
Roggertogger <mamunofficialbox@gmail.com>
2025-01-24 19:18:03 +0600

    practiced a qus with constructor and method

--- practice.py
+++ practice.py
@@ -29,7 +29,29 @@ class student:
         self.f_name = f_name
         print("constructor always work first")
 
+    def method_1(self):
+        print("this is the ", self.f_name)
+
 
 s1 = student(213311035, "Bottle")
 print(s1.id)
 print(s1.f_name)
+s1.method_1()
+
+# practice qus (make a cons and pass the name of a student and his 3 sub marks and make another method to calculate the average of the marks)
+
+
+class student:
+    def __init__(self, name, sub1, sub2, sub3):
+        self.name = name
+        self.sub1 = sub1
+        self.sub2 = sub2
+        self.sub3 = sub3
+
+    def func(self):
+        return (self.sub1 + self.sub2 + self.sub3) / 3
+
+
+s1 = student("Leon mask", 5, 5, 5)
+print(s1.sub1, s1.sub2, s1.sub3)
+print(s1.func())
