a30b3b8d5d054651ee746248332ed7cc39a8f03a
Roggertogger <mamunofficialbox@gmail.com>
2025-01-24 18:33:58 +0600

    class obj and constructor learned

--- practice.py
+++ practice.py
@@ -15,5 +15,21 @@ for i in range(2):
 mark = [21, 23, 35, 36]
 
 mark.append(32)
-mark.insert(2,43)
+mark.insert(2, 43)
 print(mark)
+
+# class and object with constructor
+
+
+class student:
+    name = "Apple"
+
+    def __init__(self, id, f_name):
+        self.id = id
+        self.f_name = f_name
+        print("constructor always work first")
+
+
+s1 = student(213311035, "Bottle")
+print(s1.id)
+print(s1.f_name)
